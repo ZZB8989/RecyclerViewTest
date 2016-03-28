7b4e8d6d102041f00927567dd6d653b997dafa73
ZZB <realbo89@outlook.com>
2016-03-28 11:46:31 +0800

    JsonHelper添加方法

--- app/src/main/java/com/example/administrator/recyclerviewtest/JsonHelper.java
+++ app/src/main/java/com/example/administrator/recyclerviewtest/JsonHelper.java
@@ -1,8 +1,11 @@
 package com.example.administrator.recyclerviewtest;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,8 +13,23 @@ import java.util.List;
  * Created by Administrator on 2016/3/21.
  */
 public class JsonHelper {
-
-
+    private static Gson sGson;
+    private static Gson getGson() {
+        if (sGson == null) {
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            sGson = gsonBuilder.create();
+        }
+        return sGson;
+    }
+    public static <T> T jsonToObject(String json, Type tokenType) {
+        T t = null;
+        try {
+            t = getGson().fromJson(json, tokenType);
+        }catch (JsonSyntaxException e){
+            e.printStackTrace();
+        }
+        return t;
+    }
 
     public static <T>T toObject(String jsonString,Class<T> mclass)
     {
